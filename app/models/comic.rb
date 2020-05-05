8d9d92ec8b78e30d42e5882e68d93a1c5b0a2005
github username <github email address>
2020-05-05 15:33:17 -0400

    comic instances

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,8 +2,4 @@ class Comic < ActiveRecord::Base
     belongs_to :series
     has_many :character_comics
     has_many :characters, through: :character_comics
-
-    def self.write
-        puts "comic"
-    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
 black_panther = Character.find_or_create_by(name: "Black Panther", alias: "T'Challa")
 spider_man = Character.find_or_create_by(name: "Spiderman", alias: "Peter Parker")
+
+avengers_1 = Comic.find_or_create_by(name: "Avengers 1", publish_date: 1998)
+avengers_2 = Comic.find_or_create_by(name: "Avengers 2", publish_date: 2018)
