9fd1617e7b727a696b608139fa35ebc7d3792dcc
github username <github email address>
2020-05-05 14:15:56 -0400

    create series table migration

--- bin/comic.rb
+++ bin/comic.rb
@@ -1,6 +1,6 @@
 class Comic < ActiveRecord::Base 
     belongs_to :series
-    belongs_to :character_comics
+    belongs_to :character_comic
     has_many :characters, through: character_comics
     
     def self.write
