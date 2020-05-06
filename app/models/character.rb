4d0fd716af38d198bc3387e84d2552a6367bc199
github username <github email address>
2020-05-06 11:31:14 -0400

    comic_id helper method

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,18 +6,18 @@ class Character < ActiveRecord::Base
         self.all.map {|character| character.alias}
     end
 
-    def authors
+    def comic_id
         charcom = CharacterComic.all.select {|cc| cc.character_id == self.id}
         com_ids = charcom.map {|comic| comic.comic_id}
         comics = Comic.all.select {|comic| com_ids.include?(comic.id)}
-        authors = comics.map {|comic| comic.author}
+    end
+
+    def authors
+        comic_id.map {|comic| comic.author}
     end
 
     def series
-        charcom = CharacterComic.all.select {|cc| cc.character_id == self.id}
-        com_ids = charcom.map {|comic| comic.comic_id}
-        comics = Comic.all.select {|comic| com_ids.include?(comic.id)}
-        series_ids = comics.map {|comic| comic.series_id}
+        series_ids = comic_id.map {|comic| comic.series_id}
         series = Series.all.select {|series| series_ids.include?(series.id)}
     end
 
