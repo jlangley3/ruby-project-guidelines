268b95d1349d3df57214834b41dd0522af9a741b
github username <github email address>
2020-05-06 09:22:10 -0400

    character series and author methods

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,15 +8,17 @@ class Character < ActiveRecord::Base
 
     def authors
         charcom = CharacterComic.all.select {|cc| cc.character_id == self.id}
-        char_ids = charcom.map {|comic| comic.comic_id}
-        comics = Comic.all.select {|comic| char_ids.include?(comic.id)}
+        com_ids = charcom.map {|comic| comic.comic_id}
+        comics = Comic.all.select {|comic| com_ids.include?(comic.id)}
         authors = comics.map {|comic| comic.author}
     end
 
     def series
         charcom = CharacterComic.all.select {|cc| cc.character_id == self.id}
-        char_ids = charcom.map {|comic| comic.comic_id}
-        comics = Comic.all.select {|comic| char_ids.include?(comic.id)}
+        com_ids = charcom.map {|comic| comic.comic_id}
+        comics = Comic.all.select {|comic| com_ids.include?(comic.id)}
+        series_ids = comics.map {|comic| comic.series_id}
+        series = Series.all.select {|series| series_ids.include?(series.id)}
     end
 
 end
