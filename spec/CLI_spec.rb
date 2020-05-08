d8e0cdf9ddec4b2f684f5cafb68adb14547cb0fc
saimaja <saimaakhtar@gmail.com>
2020-05-08 13:07:26 -0400

    passing character rspecs

--- spec/CLI_spec.rb
+++ spec/CLI_spec.rb
@@ -1,13 +1,31 @@
 require_relative "spec_helper"
+Character.destroy_all
 
 describe "Character" do
 
     it "is able to create a new character" do
-        white_panther = Character.create(name: "White Panther", alias: "T-Pain")
-        expect(Character.find_by(name: "White Panther", alias: "T-Pain")).to eq(white_panther)
+        yellow_panther = Character.create(name: "Yellow Panther", alias: "T-Bone")
+        expect(Character.find_by(name: "Yellow Panther", alias: "T-Bone")).to eq(yellow_panther)
+    end
+
+    it "has a name" do 
+        superwoman = Character.create(name: "Superwoman", alias: "Cam")
+        expect(Character.find_by(name: "Superwoman")).to eq(superwoman)
+    end
+
+    it "has an alias" do
+        wonderwoman = Character.create(name: "Wonderman", alias: "Diane")
+        expect(Character.find_by(alias: "Diane")).to eq(wonderwoman)
+    end
+
+    it "has a list of comics for each character" do
+        sam = Character.create(name: "Sam", alias: "Samantha")
+        sams_series = Series.create(name: "Sam's Series", description: "This is Sam's series description.")
+        sams_comic = Comic.create(name: "Sam's Comic", publish_date: 1998, series: sams_series, author: "Anon", storyline: "This is Sam's storyline.")
+        sam.comics << sams_comic
+        expect(sam.comics.include?(sams_comic)).to eq(true)
     end
    
-
 end
 
 
