b91aab4bd6e99e5fa89bc013824c0401e786cf46
saimaja <saimaakhtar@gmail.com>
2020-05-08 12:40:19 -0400

    passed first rspec test

--- spec/CLI_spec.rb
+++ spec/CLI_spec.rb
@@ -1,26 +1,36 @@
-require "spec_helper"
+require_relative "spec_helper"
 
 describe "Character" do
 
     it "is able to create a new character" do
-        white_panther = Character.new(name: "White Panther", alias: "T-Pain")
-        expect(white_panther.name).to eq("White Panther")
+        white_panther = Character.create(name: "White Panther", alias: "T-Pain")
+        expect(Character.find_by(name: "White Panther", alias: "T-Pain")).to eq(white_panther)
     end
+   
 
 end
 
-describe "#first_name" do 
-it "has a first name" do 
-    student.first_name = "Steve"
-    expect(student.first_name).to eq("Steve")
-end
-end
 
-describe "#first_name" do
-it "has a first name" do
-student.first_name = "Steve
 
-describe "add_student" do
-it 'is able to add a student' do
-    @school.add_student("AC Slater", 10)
-    expect(@school.roster).to eq({10 => ["AC Slater"]})     end
+
+
+
+
+
+
+
+# describe "#first_name" do 
+# it "has a first name" do 
+#     student.first_name = "Steve"
+#     expect(student.first_name).to eq("Steve")
+# end
+# end
+
+# describe "#first_name" do
+# it "has a first name" do
+# student.first_name = "Steve
+
+# describe "add_student" do
+# it 'is able to add a student' do
+#     @school.add_student("AC Slater", 10)
+#     expect(@school.roster).to eq({10 => ["AC Slater"]})     end
